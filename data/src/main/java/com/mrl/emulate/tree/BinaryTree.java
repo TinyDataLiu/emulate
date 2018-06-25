7db3d8e667c5205cde9a64fa23952a57d1c80f3f
liuchun <1360737271@qq.com>
2018-06-25 20:31:08 +0800

    树结构学习 1360737271@qq.com

---
+++ data/src/main/java/com/mrl/emulate/tree/BinaryTree.java
@@ -0,0 +1,27 @@
+package com.mrl.emulate.tree;
+
+/**
+ * @author liucun
+ * @Description: 普通二叉树
+ * @date 2018/6/2520:26
+ */
+public class BinaryTree<T> {
+
+
+    /**
+     * 根节点
+     */
+    private BinaryTree<T> root;
+    /**
+     * 左子节点
+     */
+    private BinaryTree<T> left;
+    /**
+     * 右子节点
+     */
+    private BinaryTree<T> right;
+    /**
+     * 数据
+     */
+    private T data;
+}
