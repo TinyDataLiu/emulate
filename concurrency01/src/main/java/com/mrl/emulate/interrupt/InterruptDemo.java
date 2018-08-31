4b8cb9c530f1b10672fe1478a014e483311be0f8
liuchun <1360737271@qq.com>
2018-08-31 15:07:35 +0800

    线程复位 1360737271@qq.com

--- concurrency01/src/main/java/com/mrl/emulate/interrupt/InterruptDemo.java
+++ concurrency01/src/main/java/com/mrl/emulate/interrupt/InterruptDemo.java
@@ -15,19 +15,47 @@ public class InterruptDemo {
 
     private static int num = 0;
 
+    public static void main(String[] args) throws InterruptedException {
+        //stop();
+        reset();
+    }
+
+
     /**
-     * 优雅的停止
+     * 线程停止
      *
-     * @param args
      * @throws InterruptedException
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void stop() throws InterruptedException {
         Thread thread = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted()) {
                 num++;
             }
             logger.info("num:" + num);
         }, "interrupt");
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        thread.interrupt();
+    }
+
+
+    /**
+     * 线程复位
+     *
+     * @throws InterruptedException
+     * @see Thread#interrupt()
+     */
+    public static void reset() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                boolean status = Thread.currentThread().isInterrupted();
+                if (status) {
+                    logger.info("before:" + status);
+                    Thread.interrupted();
+                    logger.info("after:" + Thread.currentThread().isInterrupted());
+                }
+            }
+        }, "reset");
         thread.start();
         TimeUnit.SECONDS.sleep(1);
         thread.interrupt();
