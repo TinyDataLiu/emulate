0c189269c8a2b3d604b48a08d462878a10c58c83
liuchun <1360737271@qq.com>
2018-09-10 10:58:40 +0800

    方法区内存溢出 测试
    1360737271@qq.com

--- jvm001/src/main/java/com/mrl/emulate/outofmemoryerror/JavaMethodAreaOOM.java
+++ jvm001/src/main/java/com/mrl/emulate/outofmemoryerror/JavaMethodAreaOOM.java
@@ -7,6 +7,13 @@ import net.sf.cglib.proxy.MethodProxy;
 import java.lang.reflect.Method;
 
 /**
+ * 这里是方法区内存溢出
+ * <p>
+ * <p>
+ * 设置 1.8 后不起作用，之后所有代码将基于 JDK1.7
+ * -XX:PermSize=20M
+ * -XX:MaxPermSize=20M
+ *
  * @author liuchun
  * @date 2018/9/10 10:35
  */
