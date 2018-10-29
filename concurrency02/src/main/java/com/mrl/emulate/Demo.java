56190ef5c2c26b2cdf6f43d2ede3531418f373af
liuchun <1360737271@qq.com>
2018-10-29 16:29:13 +0800

    分析以及回顾
    1360737271@qq.com

---
+++ concurrency02/src/main/java/com/mrl/emulate/Demo.java
@@ -0,0 +1,14 @@
+package com.mrl.emulate;
+
+/**
+ * @author liuchun
+ * @date 2018/10/29 15:31
+ */
+public class Demo {
+
+    static volatile int i;
+
+    public static void main(String[] args) {
+        i = 10;
+    }
+}
