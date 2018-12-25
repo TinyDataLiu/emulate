e94425669ef1a1584de512e173a85cb5ecca4da2
liuchun <1360737271@qq.com>
2018-12-25 10:57:06 +0800

    传入一些笔记
    1360737271@qq.com

---
+++ jvm001/src/main/java/com/mrl/emulate/TestClazz.java
@@ -0,0 +1,17 @@
+package com.mrl.emulate;
+
+/**
+ * @author liuchun
+ * @date 2018/12/13 14:16
+ */
+public class TestClazz {
+
+    protected int m;
+
+
+    @Deprecated
+    public int inc() {
+        return m + 1;
+    }
+
+}
