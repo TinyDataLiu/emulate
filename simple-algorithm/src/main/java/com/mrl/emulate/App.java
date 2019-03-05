8dc17790e56d33137790e903a29f9042110a0654
liuchun <1360737271@qq.com>
2019-03-05 14:45:28 +0800

    间接引用不用初始化类的情况 1360737271@qq.com

--- simple-algorithm/src/main/java/com/mrl/emulate/App.java
+++ simple-algorithm/src/main/java/com/mrl/emulate/App.java
@@ -2,12 +2,18 @@ package com.mrl.emulate;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String ss = "123123123";
+
+        /*do {
+            //自动计算PC寄存器的值加1；
+            //根据PC寄存器的指示位置，从字节码流中取出操作码；
+            //if（字节码存在操作数）从字节码流中取出操作数；
+            //执行操作码所定义的操作；
+        } while (//字节码流长度＞0);*/
     }
+
+
 }
