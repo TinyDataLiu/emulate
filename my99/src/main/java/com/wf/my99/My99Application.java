491bf7536720b1ce4a20e41bba1bf134f398dce5
liuchun <1360737271@qq.com>
2019-03-18 16:06:44 +0800

    万蜂 my99

---
+++ my99/src/main/java/com/wf/my99/My99Application.java
@@ -0,0 +1,13 @@
+package com.wf.my99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class My99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(My99Application.class, args);
+	}
+
+}
