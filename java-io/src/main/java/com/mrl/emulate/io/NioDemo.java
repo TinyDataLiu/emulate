d1af43da3aff0f68a04acfb09d2e58b385168e6a
liuchun <1360737271@qq.com>
2020-02-26 15:35:26 +0800

    NIO demo

---
+++ java-io/src/main/java/com/mrl/emulate/io/NioDemo.java
@@ -0,0 +1,30 @@
+package com.mrl.emulate.io;
+
+import java.io.*;
+
+/**
+ * NIO
+ *
+ * @author liuchun
+ * @date 2020/02/26  15:25
+ */
+public class NioDemo {
+    private static final File file = new File("user.txt");
+
+    public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("user.txt")))) {
+            for (String line = reader.readLine(); null != line && !line.equals(""); line = reader.readLine()) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+
+}
