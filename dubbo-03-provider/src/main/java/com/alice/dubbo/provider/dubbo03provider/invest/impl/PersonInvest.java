2891cd0afa75550e2cac222bc6d36b1b69d864ca
卖小姑娘的火柴 <1360737271@qq.com>
2020-03-06 18:28:02 +0800

    Dubbo SPI 使用

--- dubbo-03-provider/src/main/java/com/alice/dubbo/provider/dubbo03provider/invest/impl/PersonInvest.java
+++ dubbo-03-provider/src/main/java/com/alice/dubbo/provider/dubbo03provider/invest/impl/PersonInvest.java
@@ -2,8 +2,10 @@ package com.alice.dubbo.provider.dubbo03provider.invest.impl;
 
 import com.alice.dubbo.provider.dubbo03provider.invest.IInvest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 public class PersonInvest implements IInvest {
     @Override
     public String invest(Double amt) {
--- dubbo-03-provider/src/test/java/com/alice/dubbo/provider/dubbo03provider/invest/IInvestTest.java
+++ dubbo-03-provider/src/test/java/com/alice/dubbo/provider/dubbo03provider/invest/IInvestTest.java
@@ -1,17 +1,30 @@
 package com.alice.dubbo.provider.dubbo03provider.invest;
 
-import org.apache.dubbo.common.extension.ExtensionLoader;
+import com.alice.dubbo.provider.dubbo03provider.Dubbo03ProviderApplication;
+import com.alice.dubbo.provider.dubbo03provider.invest.impl.PersonInvest;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class IInvestTest {
 
     @Test
     void invest() {
-        IInvest personInvest = ExtensionLoader.getExtensionLoader(IInvest.class).getExtension("personInvest");
-        personInvest.invest(1000D);
+//        IInvest personInvest = ExtensionLoader.getExtensionLoader(IInvest.class).getExtension("personInvest");
+//        personInvest.invest(1000D);
+//
+//        IInvest defaultExtension = ExtensionLoader.getExtensionLoader(IInvest.class).getDefaultExtension();
+//        String invest = defaultExtension.invest(10000D);
+//        System.out.println(invest);
+//
 
-        IInvest defaultExtension = ExtensionLoader.getExtensionLoader(IInvest.class).getDefaultExtension();
-        String invest = defaultExtension.invest(10000D);
-        System.out.println(invest);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Dubbo03ProviderApplication.class);
+
+        PersonInvest bean = context.getBean(PersonInvest.class);
+
+
+
+
+        bean.invest(1000D);
+
     }
 }
