14ccde4c561ecd5ecaac49c89bd19702acdb09a3
卖小姑娘的火柴 <1360737271@qq.com>
2020-03-19 15:51:59 +0800

    nacos 注册中心，配置中心

--- java-io/src/main/java/com/mrl/emulate/App.java
+++ java-io/src/main/java/com/mrl/emulate/App.java
@@ -5,9 +5,7 @@ package com.mrl.emulate;
  */
 public class App {
     public static void main(String[] args) {
-
         int n = 16;
-
         System.out.println(n - (n >>> 2));
         System.out.println(n * 0.75);
     }
