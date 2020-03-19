f7f77e24c0a7b6e087a100a17a0e3e471601cecf
卖小姑娘的火柴 <1360737271@qq.com>
2020-03-19 15:51:41 +0800

    nacos 注册中心，配置中心

--- java-io/src/main/java/com/mrl/emulate/App.java
+++ java-io/src/main/java/com/mrl/emulate/App.java
@@ -2,12 +2,13 @@ package com.mrl.emulate;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int n = 16;
+
+        System.out.println(n - (n >>> 2));
+        System.out.println(n * 0.75);
     }
 }
